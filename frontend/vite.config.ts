cdcec92a4d4c1d7579b3478a0b0a5c2009f6f15c
Lewismosage <lewismosage@users.noreply.github.com>
2025-07-31 11:58:30 +0300

    Fixed Vercel deployment configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        '@stripe/stripe-js',
-        '@stripe/react-stripe-js'
-      ],
-      input: './src/main.tsx' 
-    },
-    outDir: './dist', 
-    assetsDir: 'assets',
-    emptyOutDir: true
+  optimizeDeps: {
+    exclude: ['lucide-react'],
   },
-  base: '/',
 });
